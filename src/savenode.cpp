38bd794b564bfea6c382d4d6dfdf033232d13696
刘权祥 <15115852131@163.com>
2020-12-10 17:20:07 +0800

    支持动态修改设备端口号

--- src/savenode.cpp
+++ src/savenode.cpp
@@ -6,7 +6,7 @@ int main(int argc, char **argv)
 {
     ros::init(argc, argv, "VideoSaver");
 
-    Saver saver(2, argv[1]);
+    Saver saver(std::stoi(argv[1]), argv[2]);
 
     saver.saveVideo();
 
